1b3342c4765f92eabb96666a8d65a5445ab2e602
mdrumon42243 <mdrumonkhan48519@gmail.com>
2022-04-08 12:20:17 +0600

    login and signup system implementation done

--- src/App.js
+++ src/App.js
@@ -3,48 +3,95 @@ import app from './firebase.init';
 import facebookImage from './images/facebook.svg';
 import googleImage from './images/google.svg';
 import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
 
 const auth = getAuth(app);
 
-const handleFacebookEvent = () => {
-
-
-  // Facebook authentication handler seating 
-  const facebookProvider = new FacebookAuthProvider();
-  signInWithPopup(auth, facebookProvider)
-    .then(result => {
-      console.log(result.user);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-}
-
-// Google authentication handler seating
-const googleProvider = new GoogleAuthProvider();
-const handleGoogelEvent = () => {
-  signInWithPopup(auth, googleProvider)
-    .then(result => {
-      console.log(result.user);
-    })
-    .catch(error => {
-      console.log(error);
-    })
-}
-
-
 function App() {
+  const [registered, setRegistered] = useState(false);
+
+  const handleLogInAndSignUp = event => {
+    setRegistered(event.target.checked);
+    console.log(event.target.checked);
+  }
+
+  const handleFacebookEvent = () => {
+
+    // Facebook authentication handler seating 
+    const facebookProvider = new FacebookAuthProvider();
+    signInWithPopup(auth, facebookProvider)
+      .then(result => {
+        console.log(result.user);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  // Google authentication handler seating
+  const googleProvider = new GoogleAuthProvider();
+  const handleGoogelEvent = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        console.log(result.user);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+
   return (
     <div>
       <div className='sign-in-form'>
-        <div onClick={handleFacebookEvent} className="facebook-auth-container">
-          <img className='facebook-image ' src={facebookImage} alt="" />
-          <button>SIGN IN WITH FACEBOOK</button>
+        <h2 className='text-green'>Please {registered ? 'Log In' : 'Sign In'}!!!</h2>
+        <div className="get-user-info">
+          <form>
+            <div className='registered'>
+              {
+                !registered ? <input
+                  className='name- input'
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder='Enter your name' /> : ''
+              }
+              <input
+                className='email-input input'
+                type="email"
+                name="email" id="email"
+                placeholder='Enter your email' />
+              <input
+                className='password-input input'
+                type="password"
+                name="password"
+                id="password"
+                placeholder='Enter your password' />
+            </div>
+            <input onChange={handleLogInAndSignUp} type="checkbox" name="checkbox" id="checkbox" />
+            <label htmlFor="checkbox"> {
+              registered ? 'Create new account' : 'Already have a account?'
+            }</label>
+            <br />
+          </form>
         </div>
-        <div onClick={handleGoogelEvent} className="google-auth-container">
-          <img className='google-image ' src={googleImage} alt="" />
-          <button>SIGN IN WITH GOOGLE</button>
-        </div>
+
+        {
+          registered ? <button>Log In</button> :
+            <div>
+              <div onClick={handleFacebookEvent} className="facebook-auth-container">
+                <img className='facebook-image ' src={facebookImage} alt="" />
+                <button>SIGN IN WITH FACEBOOK</button>
+              </div>
+              <div onClick={handleGoogelEvent} className="google-auth-container">
+                <img className='google-image ' src={googleImage} alt="" />
+                <button>SIGN IN WITH GOOGLE</button>
+              </div>
+              <div>
+                <button>SIGN IN</button>
+              </div>
+            </div>
+        }
       </div>
     </div>
   );

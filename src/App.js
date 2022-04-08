3789edd1b0aa1c32518c98e948e26110d64875b1
mdrumon42243 <mdrumonkhan48519@gmail.com>
2022-04-08 11:09:53 +0600

    google sign in done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
 import app from './firebase.init';
-import Image from './images/facebook.svg'
-import { FacebookAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-
+import facebookImage from './images/facebook.svg';
+import googleImage from './images/google.svg';
+import { FacebookAuthProvider, getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const auth = getAuth(app);
 
-const facebookProvider = new FacebookAuthProvider();
-
 const handleFacebookEvent = () => {
+
+
+  // Facebook authentication handler seating 
+  const facebookProvider = new FacebookAuthProvider();
   signInWithPopup(auth, facebookProvider)
+    .then(result => {
+      console.log(result.user);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+}
+
+// Google authentication handler seating
+const googleProvider = new GoogleAuthProvider();
+const handleGoogelEvent = () => {
+  signInWithPopup(auth, googleProvider)
     .then(result => {
       console.log(result.user);
     })
@@ -24,8 +38,12 @@ function App() {
     <div>
       <div className='sign-in-form'>
         <div onClick={handleFacebookEvent} className="facebook-auth-container">
-          <img className='facebook-image ' src={Image} alt="" />
+          <img className='facebook-image ' src={facebookImage} alt="" />
           <button>SIGN IN WITH FACEBOOK</button>
+        </div>
+        <div onClick={handleGoogelEvent} className="google-auth-container">
+          <img className='google-image ' src={googleImage} alt="" />
+          <button>SIGN IN WITH GOOGLE</button>
         </div>
       </div>
     </div>
